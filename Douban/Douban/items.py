060f181b2b4bd1f56bdc2f428d0fd4689d290177
hangxigood <hangxigood@gmail.com>
2020-06-30 21:20:45 +0800

    创建项目，爬取豆瓣top250电影信息。

---
+++ Douban/Douban/items.py
@@ -0,0 +1,16 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class DoubanItem(scrapy.Item):
+    # define the fields for your item here like:
+    name = scrapy.Field()
+    year = scrapy.Field()
+    score = scrapy.Field()
+    director = scrapy.Field()
+    classification = scrapy.Field()
+    actor = scrapy.Field()
